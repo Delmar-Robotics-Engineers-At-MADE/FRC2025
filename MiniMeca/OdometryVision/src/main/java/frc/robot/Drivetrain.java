a400e8b1371a9c964f02a26c4e7984d38e0c7957
Riley Wagner <rpw.32@live.com>
2025-02-01 14:59:28 -0600

    added Y coord to estimated pose

--- MiniMeca/OdometryVision/src/main/java/frc/robot/Drivetrain.java
+++ MiniMeca/OdometryVision/src/main/java/frc/robot/Drivetrain.java
@@ -220,7 +220,7 @@ public class Drivetrain {
     if (visionOptional.isPresent()) {
       EstimatedRobotPose visionPose = visionOptional.get(); 
 
-      System.out.println("X: " + visionPose.estimatedPose.toPose2d().getX());
+      System.out.println("X: " + String.format("%.6f", visionPose.estimatedPose.toPose2d().getX()) + "Y: " + String.format("%.6f", visionPose.estimatedPose.toPose2d().getY()));
 
       m_poseEstimator.addVisionMeasurement(
         visionPose.estimatedPose.toPose2d(),
