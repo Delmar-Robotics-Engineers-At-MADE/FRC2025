1ee91636ba4ed4064f0dfb4b0053d5fb70a614db
Matt Snyder <slylock2@mac.com>
2025-03-06 15:10:18 -0600

    preparing for more subsystems

--- Titan/MAXSwerve-Java-Template-2025-main/src/main/java/frc/robot/commands/SetBlinkinColorCmd.java
+++ Titan/MAXSwerve-Java-Template-2025-main/src/main/java/frc/robot/commands/SetBlinkinColorCmd.java
@@ -8,16 +8,18 @@ import frc.robot.subsystems.Blinkin;
 import edu.wpi.first.wpilibj2.command.Command;
 
 /** An example command that uses an example subsystem. */
-public class AllianceColorCmd extends Command {
+public class SetBlinkinColorCmd extends Command {
   private final Blinkin m_blinkin;
+  private double m_color;
 
   /**
    * Creates a new ExampleCommand.
    *
    * @param subsystem The subsystem used by this command.
    */
-  public AllianceColorCmd(Blinkin blinkin) {
+  public SetBlinkinColorCmd(Blinkin blinkin, double color) {
     m_blinkin = blinkin;
+    m_color = color;
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(blinkin);
   }
@@ -25,19 +27,21 @@ public class AllianceColorCmd extends Command {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    // just need to do this one, with one check of current position
-    m_blinkin. 
+    // just need to do this once
+    m_blinkin.setColor(m_color); 
   }
   
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    // don't need to do anything, as color was set in initialize
+  }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {}
 
-  // Returns true when the command should end.
+  // Run until interrupted
   @Override
   public boolean isFinished() {
     return false;
